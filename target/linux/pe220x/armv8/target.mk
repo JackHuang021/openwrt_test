9ab0a4349d2fb3d387c011334f016d13ccef5a1e
Huangjie <huangjie1663@phytium.com.cn>
2023-11-01 09:30:00 +0800

    pe220x: add phytium pe220x platform
    Signed-off-by: Huangjie <huangjie1663@phytium.com.cn>

---
+++ target/linux/pe220x/armv8/target.mk
@@ -0,0 +1,8 @@
+ARCH:=aarch64
+SUBTARGET:=armv8
+BOARDNAME:=PE220x boards
+
+define Target/Description
+	Build firmware image for Phytium PE220x devices.
+	This firmware features a 64 bit kernel.
+endef
---
+++ target/linux/pe220x/image/armv8.mk
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+define Device/e2000d-demo-board
+  DEVICE_VENDOR := Phytium
+  DEVICE_MODEL := E200D Demo Board
+  SOC := PE2202
+endef
+TARGET_DEVICES += e2000d-demo-board
